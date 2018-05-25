d2c1d473b216e910c4419fd7b56094bfccb2c225
lucien1011 <lucien1011@gmail.com>
2018-05-25 17:26:24 +0200

    Added cert to access files with LSF

--- TTHAnalysis/macros/lxbatch_runner.sh
+++ TTHAnalysis/macros/lxbatch_runner.sh
@@ -3,6 +3,7 @@ if [[ "$VO_CMS_SW_DIR" != "" ]] && test -f $VO_CMS_SW_DIR/cmsset_default.sh; the
   source $VO_CMS_SW_DIR/cmsset_default.sh
 fi;
 export SCRAM_ARCH=slc6_amd64_gcc530
+export X509_USER_PROXY=/afs/cern.ch/user/k/klo/cert/x509up_u41672
 WORK=$1; shift
 SRC=$1; shift
 cd $SRC; 
