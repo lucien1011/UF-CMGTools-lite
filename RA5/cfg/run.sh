62c7f4271d3f03c3d67962eb71bef4e6b88ccdc3
lucien1011 <lucien1011@gmail.com>
2018-08-29 12:40:59 -0400

    Sync file

--- RA5/cfg/run.sh
+++ RA5/cfg/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 #heppy TEST_01 run_BkgMC2016_cfg.py -N 1000 -f
-heppy TEST_Sig run_PrivSignalMC2016_cfg.py -N 1000 -f
+#heppy TEST_Sig run_PrivSignalMC2016_cfg.py -N 1000 -f
+#heppy TEST_Data run_Data2016_cfg.py -N 1000 -f -o test=ra5-sync-mc
+heppy SyncMC2016 run_BkgMC2016_cfg.py -N -1 -f -o test=ra5-sync-mc
